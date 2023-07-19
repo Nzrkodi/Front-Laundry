25c9d6da4336b891f750deb2f5775434bee9a47d
nzrkodi <nzrkodi@gmail.com>
2023-07-19 13:40:03 +0800

    create sidebar

--- src/views/skelton/SideBar.vue
+++ src/views/skelton/SideBar.vue
@@ -1,61 +1,56 @@
 <template>
-  <div id="sidebar" class='active'>
+  <div id="sidebar" class="active">
     <div class="sidebar-wrapper active">
       <div class="sidebar-header">
         <img src="../../assets/images/logoku.png" style="transform: scale(1.6);" class="ms-4" alt="no logo" srcset="">
       </div>
       <div class="sidebar-menu">
         <ul class="menu">
-          <li class='sidebar-title'>Main Menu</li>
-          <li class="sidebar-item active ">
-            <a href="index.html" class='sidebar-link'>
+          <li class="sidebar-title">Home</li>
+          <li class="sidebar-item active">
+            <router-link :to="{ name: 'home' }" class="sidebar-link">
               <span>Dashboard</span>
-            </a>
+            </router-link>
+
           </li>
-          <li class="sidebar-item  has-sub">
-            <a href="#" class='sidebar-link'>
-              <span>Data</span>
-            </a>
-            <ul class="submenu ">
-              <li>
-                <a href="form-element-input-group.html">Transaksi</a>
-              </li>
-              <li>
-                <a href="form-element-select.html">Orders</a>
-              </li>
-              <li>
-                <a href="form-element-radio.html">Detail Orders</a>
-              </li>
-              <li>
-                <a href="form-element-checkbox.html">Jenis Paket</a>
-              </li>
-              <li>
-                <a href="form-element-textarea.html">Cucian Satuan</a>
-              </li>
-            </ul>
+          <li class="sidebar-title">Data</li>
+          <li class="sidebar-item">
+            <router-link :to="{ name: 'order' }" class="sidebar-link">
+              <span>Order</span>
+            </router-link>
           </li>
-          <li class="sidebar-item  ">
-            <a href="table.html" class='sidebar-link'>
+          <li class="sidebar-item">
+            <router-link :to="{ name: 'transaksi' }" class="sidebar-link">
+              <span>Transaksi</span>
+            </router-link>
+          </li>
+          <li class="sidebar-item">
+            <router-link :to="{ name: 'jenis' }" class="sidebar-link">
+              <span>Jenis</span>
+            </router-link>
+          </li>
+          <li class="sidebar-item">
+            <router-link :to="{ name: 'paket' }" class="sidebar-link">
+              <span>Paket</span>
+            </router-link>
+          </li>
+          <li class="sidebar-title">Setup</li>
+          <li class="sidebar-item">
+            <a href="table.html" class="sidebar-link">
               <span>Report</span>
             </a>
           </li>
-          <li class="sidebar-item  has-sub">
-            <a href="#" class='sidebar-link'>
-              <span>Menage Akun</span>
+          <li class="sidebar-item">
+            <a href="table.html" class="sidebar-link">
+              <span>Manage Akun</span>
             </a>
-            <ul class="submenu ">
-              <li>
-                <a href="form-element-input-group.html">Client</a>
-              </li>
-              <li>
-                <a href="form-element-select.html">Pegawai</a>
-              </li>
-            </ul>
           </li>
         </ul>
       </div>
-      <button class="sidebar-toggler btn x"></button>
     </div>
   </div>
 </template>
-<script setup></script>
+
+<script setup>
+
+</script>
